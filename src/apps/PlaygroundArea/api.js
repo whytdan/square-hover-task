bd5171ee2f6f86651c559964393796baf4ac6160
whytdan <aktan.r.a@gmail.com>
2021-05-17 02:09:34 +0600

    off eslint "no-undef" rule

--- src/apps/PlaygroundArea/api.js
+++ src/apps/PlaygroundArea/api.js
@@ -1,6 +1,4 @@
 export const fetchGameModes = async () => {
-  /* global fetch */
-
   const response = await fetch('https://demo1030918.mockable.io/');
   const data = await response.json();
   return data;
